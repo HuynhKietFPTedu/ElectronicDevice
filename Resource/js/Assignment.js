017aac55ae41ac303716af217db525332e19a9c5
TuKi2901 <huynhtrantuankiet2901@gmail.com>
2024-03-02 15:48:54 +0700

    a

---
+++ Resource/js/Assignment.js
@@ -0,0 +1,314 @@
+var products =
+    [
+        {
+            "amount": "1",
+            "status": "old",
+            "ID": "1",
+            "name": "Samsung Galaxy S9",
+            "price": "300",
+            "sale": "20",
+            "quantity": "3142",
+            "sold": "1414",
+            "category": "Điện thoại",
+            "date": "19-4-2006",
+            "country": "Hàn Quốc",
+            "link": "Resource/images/1.png"
+        },
+        {
+            "amount": "1",
+            "status": "new",
+            "ID": "2",
+            "name": "Samsung Galaxy S9",
+            "price": "800",
+            "sale": "15",
+            "quantity": "2500",
+            "sold": "1200",
+            "category": "Điện thoại",
+            "date": "12-8-2022",
+            "country": "Mỹ",
+            "link": "Resource/images/2.png"
+        },
+
+        {
+            "amount": "1",
+            "status": "new",
+            "ID": "3",
+            "name": "Xiaomi Redmi Note 9",
+            "price": "250",
+            "sale": "10",
+            "quantity": "1800",
+            "sold": "800",
+            "category": "Điện thoại",
+            "date": "5-3-2023",
+            "country": "Trung Quốc",
+            "link": "Resource/images/3.png"
+        },
+        {
+            "amount": "1",
+            "status": "old",
+            "ID": "4",
+            "name": "Sony Xperia 5 II",
+            "price": "900",
+            "sale": "20",
+            "quantity": "3000",
+            "sold": "1500",
+            "category": "Điện thoại",
+            "date": "18-11-2021",
+            "country": "Nhật Bản",
+            "link": "Resource/images/4.png"
+        },
+        {
+            "amount": "1",
+            "status": "old",
+            "ID": "5",
+            "name": "OnePlus 8T",
+            "price": "700",
+            "sale": "18",
+            "quantity": "2200",
+            "sold": "1000",
+            "category": "Điện thoại",
+            "date": "9-6-2022",
+            "country": "Ấn Độ",
+            "link": "Resource/images/5.png"
+        },
+        {
+            "amount": "1",
+            "status": "new",
+            "ID": "6",
+            "name": "Samsung Galaxy A52",
+            "price": "350",
+            "sale": "12",
+            "quantity": "2800",
+            "sold": "1300",
+            "category": "Điện thoại",
+            "date": "24-4-2023",
+            "country": "Hàn Quốc",
+            "link": "Resource/images/6.png"
+        },
+        {
+            "amount": "1",
+            "status": "old",
+            "ID": "7",
+            "name": "Huawei P40",
+            "price": "600",
+            "sale": "15",
+            "quantity": "2000",
+            "sold": "900",
+            "category": "Điện thoại",
+            "date": "7-9-2022",
+            "country": "Trung Quốc",
+            "link": "Resource/images/7.png"
+        },
+        {
+            "amount": "1",
+            "status": "new",
+            "ID": "8",
+            "name": "LG Velvet",
+            "price": "500",
+            "sale": "10",
+            "quantity": "2600",
+            "sold": "1100",
+            "category": "Điện thoại",
+            "date": "15-12-2022",
+            "country": "Hàn Quốc",
+            "link": "Resource/images/8.png"
+        },
+        {
+            "amount": "1",
+            "status": "new",
+            "ID": "9",
+            "name": "Google Pixel 5",
+            "price": "800",
+            "sale": "15",
+            "quantity": "2400",
+            "sold": "1000",
+            "category": "Điện thoại",
+            "date": "3-2-2023",
+            "country": "Mỹ",
+            "link": "Resource/images/1.png"
+        },
+        {
+            "amount": "1",
+            "status": "old",
+            "ID": "10",
+            "name": "Oppo Reno4",
+            "price": "450",
+            "sale": "10",
+            "quantity": "1800",
+            "sold": "900",
+            "category": "Điện thoại",
+            "date": "20-7-2022",
+            "country": "Trung Quốc",
+            "link": "Resource/images/2.png"
+        },
+        {
+            "amount": "1",
+            "status": "new",
+            "ID": "11",
+            "name": "Motorola Moto G Power",
+            "price": "300",
+            "sale": "12",
+            "quantity": "2100",
+            "sold": "800",
+            "category": "Điện thoại",
+            "date": "8-5-2022",
+            "country": "Mỹ",
+            "link": "Resource/images/3.png"
+        },
+        {
+            "amount": "1",
+            "status": "new",
+            "ID": "12",
+            "name": "Realme 7 Pro",
+            "price": "350",
+            "sale": "15",
+            "quantity": "2700",
+            "sold": "1200",
+            "category": "Điện thoại",
+            "date": "14-10-2022",
+            "country": "Ấn Độ",
+            "link": "Resource/images/4.png"
+        },
+        {
+            "amount": "1",
+            "status": "new",
+            "ID": "13",
+            "name": "Nokia 8.3",
+            "price": "550",
+            "sale": "18",
+            "quantity": "2300",
+            "sold": "1100",
+            "category": "Điện thoại",
+            "date": "5-9-2023",
+            "country": "Pháp",
+            "link": "Resource/images/5.png"
+        },
+        {
+            "amount": "1",
+            "status": "new",
+            "ID": "14",
+            "name": "Vivo V20",
+            "price": "400",
+            "sale": "12",
+            "quantity": "1900",
+            "sold": "1000",
+            "category": "Điện thoại",
+            "date": "22-6-2022",
+            "country": "Ấn Độ",
+            "link": "Resource/images/6.png"
+        },
+        {
+            "amount": "1",
+            "status": "new",
+            "ID": "15",
+            "name": "Xiaomi Mi 10T",
+            "price": "550",
+            "sale": "15",
+            "quantity": "2500",
+            "sold": "1300",
+            "category": "Điện thoại",
+            "date": "11-3-2023",
+            "country": "Trung Quốc",
+            "link": "Resource/images/7.png"
+        },
+        {
+            "amount": "1",
+            "status": "old",
+            "ID": "16",
+            "name": "Samsung Galaxy Note 20",
+            "price": "900",
+            "sale": "20",
+            "quantity": "2900",
+            "sold": "1500",
+            "category": "Điện thoại",
+            "date": "17-8-2022",
+            "country": "Hàn Quốc",
+            "link": "Resource/images/8.png"
+        }
+    ];
+
+
+var users =
+    [
+        {
+            "ID": "1",
+            "username": "Tao",
+            "email": "tao@gmail.com",
+            "pass": "tao",
+            "gender": "nam",
+            "birthday": "11-10-2004"
+        },
+        {
+            "ID": "2",
+            "username": "Huyen",
+            "email": "huyen@gmail.com",
+            "pass": "huyen",
+            "gender": "nữ",
+            "birthday": "23-05-1998"
+        },
+        {
+            "ID": "3",
+            "username": "Anh",
+            "email": "anh@gmail.com",
+            "pass": "anh",
+            "gender": "nam",
+            "birthday": "15-09-2001"
+        },
+        {
+            "ID": "4",
+            "username": "Mai",
+            "email": "mai@gmail.com",
+            "pass": "mai",
+            "gender": "nữ",
+            "birthday": "08-12-1995"
+        },
+        {
+            "ID": "5",
+            "username": "Nam",
+            "email": "nam@gmail.com",
+            "pass": "nam",
+            "gender": "nam",
+            "birthday": "20-03-2000"
+        },
+        {
+            "ID": "6",
+            "username": "Linh",
+            "email": "linh@gmail.com",
+            "pass": "linh",
+            "gender": "nữ",
+            "birthday": "02-07-1993"
+        },
+        {
+            "ID": "7",
+            "username": "Khoa",
+            "email": "khoa@gmail.com",
+            "pass": "khoa",
+            "gender": "nam",
+            "birthday": "14-11-1997"
+        },
+        {
+            "ID": "8",
+            "username": "Trang",
+            "email": "trang@gmail.com",
+            "pass": "trang",
+            "gender": "nữ",
+            "birthday": "27-04-1989"
+        },
+        {
+            "ID": "9",
+            "username": "Duc",
+            "email": "duc@gmail.com",
+            "pass": "duc",
+            "gender": "nam",
+            "birthday": "05-06-2002"
+        },
+        {
+            "ID": "10",
+            "username": "Thu",
+            "email": "thu@gmail.com",
+            "pass": "thu",
+            "gender": "nữ",
+            "birthday": "18-08-1990"
+        }
+    ]
+
